01b1093e42d1a0e1c1a772f0516a611e9923d2a9
tomislav-hop <Tomislav Hop>
2014-06-27 17:10:23 +0200

    Prijava
    Napravljena prijava u aplikaciju

---
+++ projekt/PI_POCETAK/PI_POCETAK/Prijava.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PI_POCETAK.Model;
+
+namespace PI_POCETAK
+{
+    public partial class Prijava : Form
+    {
+        public Prijava()
+        {
+            InitializeComponent();
+        }
+
+        private tiskaraNETEntities baza = new tiskaraNETEntities();
+
+        private void btnPrijava_Click(object sender, EventArgs e)
+        {
+            if (txtKorisnickoIme.Text == "")
+            {
+                MessageBox.Show("Niste unjeli korisničko ime!");
+                txtKorisnickoIme.Focus();
+            }
+            else if (txtLozinka.Text == "")
+            {
+                MessageBox.Show("Niste unjeli lozinku!");
+                txtLozinka.Focus();
+            }
+
+            string korisnisnickoIme = txtKorisnickoIme.Text;
+            string lozinka = txtLozinka.Text;
+
+            foreach (var item in baza.korisnici)
+            {
+                if (item.korisnicko_ime.Equals(korisnisnickoIme))
+                {
+                    if (item.lozinka.Equals(lozinka))
+                    {
+                        this.Hide();
+                        TiskaraNET forma = new TiskaraNET();
+                        forma.Show();
+                        break;
+
+                    }
+                }
+                else 
+                {
+                    MessageBox.Show("Unesli ste krivi mail ili krivu lozinku!");
+                }
+            }
+
+        }
+    }
+}
